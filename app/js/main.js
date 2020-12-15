bf7b421e40be7182a95ae78db951c2d87c56cd42
Sergey Murzak <murzak301@gmail.com>
2020-12-16 01:03:59 +0600

    Finish first page

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,12 @@
 
 let burgerBtn = document.querySelector('.burger-menu');
 let burgerItem = document.querySelectorAll('.burger-item');
+let mobileMenu = document.querySelector('.mobile__menu');
 burgerBtn.addEventListener('click',function(){
+    burgerBtn.classList.toggle('border');
     burgerItem.forEach(function(event){
         event.classList.toggle('active');
-        
+        mobileMenu.classList.toggle('active');
     })
    
 })
